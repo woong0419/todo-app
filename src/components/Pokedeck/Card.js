f450035ba55d2f89817cdf81bde5721dd3519f2f
woong0419 <woong0419@gmail.com>
2022-03-17 18:18:33 -0400

    editing modal start

--- src/components/Pokedeck/Card.js
+++ src/components/Pokedeck/Card.js
@@ -1,46 +1,55 @@
 
-import  Axios  from 'axios'
+import Axios from 'axios'
 import { useEffect } from 'react'
-import { Fragment,useState,useContext } from 'react'
+import { Fragment, useState, useContext } from 'react'
 import styles from './Card.module.css'
 import AuthContext from '../store/auth-context'
 
 const Card = (props) => {
     const ctx = useContext(AuthContext);
     const url = props.url
-    const [mobId,setMobId] = useState()
-    const [mobImg,setMobImg] = useState()
+    const [mobId, setMobId] = useState()
+    const [mobImg, setMobImg] = useState()
     let isCollected = false;
 
-    useEffect(()=>{
+    useEffect(() => {
         let isMounted = true;
-        Axios.get(url).then((res)=>{
-            if (isMounted){
+        Axios.get(url).then((res) => {
+            if (isMounted) {
                 setMobId(res.data.id)
                 setMobImg(res.data.sprites.front_default)
-               }
+            }
         })
         return () => { isMounted = false };
-    },[])
+    }, [])
 
-    // eslint-disable-next-line array-callback-return
-    ctx.collections.map((val) => {
-        if(+val === mobId){
-            isCollected = true
-        }    
-    })
+    // // eslint-disable-next-line array-callback-return
+    // ctx.collections.map((val) => {
+    //     if (+val === mobId) {
+    //         isCollected = true
+    //     }
+    // })
+    const collectionCheckHandler = () => {
+        ctx.collections.map((val) => {
+            if (+val === mobId) {
+                isCollected = true
+            }
+        }
+        )
+    }
 
     const clickHandler = (e) => {
-        if(isCollected){
-            ctx.modalHandler(mobId,true)
+        if (isCollected) {
+            ctx.modalHandler(mobId, true)
         }
     }
 
-    return(<Fragment>
+    return (<Fragment>
         {mobId && (<>
-        <div onClick={clickHandler}>
-            <img className={`${styles['deck-img']} ${isCollected && styles['collected']}`} src={mobImg} alt='pokemon-img'/>
-        </div>
+            <div onClick={clickHandler}>
+                {collectionCheckHandler()}
+                <img className={`${styles['deck-img']} ${isCollected && styles['collected']}`} src={mobImg} alt='pokemon-img' />
+            </div>
         </>)}
     </Fragment>)
 }
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,25 +1,45 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { Fragment } from 'react'
 import ReactDom from "react-dom";
 import styles from './Modal.module.css'
 
 const Backdrop = props => {
     return <div className={styles.backdrop}
-     onClick={props.modalMessageHandler}/>
+        onClick={props.modalMessageHandler} />
 }
 
 const ModalOverlay = props => {
-    const classes = props.isImg ? styles.modal : styles['modal-text']  
+    const [pokemonData, setPokemonData] = useState([])
+
+    const classes = props.isImg ? styles.modal : styles['modal-text']
     const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.children}.png`
+
+    const getPokemonData = async (id) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const data = await res.json()
+        setPokemonData(data)
+    }
+
+    useEffect(() => {
+        getPokemonData(props.children)
+    }, [])
+
     return <div className={classes}>
-        {!props.isImg ? (<div className={styles.content}>{props.children}</div>) 
-        : (<img src={url} alt='pokemon-img' />)}
-        
+        {!props.isImg ? (<div className={styles.content}>{props.children}</div>)
+            : (
+                <div className={styles['img-box']}>
+                    <img src={url} alt='pokemon-img' />
+                    {/* <h6>{pokemonData.forms.name}</h6> */}
+                    {console.log(pokemonData)}
+                </div>
+            )}
     </div>
 }
 
 const portalElement = document.getElementById('overlays')
 
-const Modal = (props) =>{
+const Modal = (props) => {
     return <Fragment>
         {ReactDom.createPortal(<Backdrop modalMessageHandler={props.displayHandler} />, portalElement)}
         {ReactDom.createPortal(<ModalOverlay isImg={props.isImg}>{props.children}</ModalOverlay>, portalElement)}
